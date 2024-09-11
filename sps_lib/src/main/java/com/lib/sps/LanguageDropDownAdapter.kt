de07e9de022cbd7c574c03a19ff8c7f6eac2b42a
deepak.gaba <deepak.gaba@payworldindia.com>
2024-09-11 11:29:23 +0530

    initial code

---
+++ sps_lib/src/main/java/com/lib/sps/LanguageDropDownAdapter.kt
@@ -0,0 +1,68 @@
+package com.lib.sps
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.TextView
+import com.example.example.ConsentLanguage
+import com.example.example.EkycDeviceList
+
+class LanguageDropDownAdapter(val context: Context, var listItemsTxt: ArrayList<ConsentLanguage>) : BaseAdapter() {
+
+
+    val mInflater: LayoutInflater = LayoutInflater.from(context)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val view: View
+        val vh: ItemRowHolder
+        if (convertView == null) {
+            view = mInflater.inflate(R.layout.item_language, parent, false)
+            vh = ItemRowHolder(view)
+            view?.tag = vh
+        } else {
+            view = convertView
+            vh = view.tag as ItemRowHolder
+        }
+
+        // setting adapter item height programatically.
+
+//        val params = view.layoutParams
+//        params.height = 500
+//        view.layoutParams = params
+
+        vh.label.text = listItemsTxt.get(position).language
+        return view
+    }
+
+    override fun getItem(position: Int): Any? {
+
+        return null
+
+    }
+
+    override fun getItemId(position: Int): Long {
+
+        return 0
+
+    }
+
+    override fun getCount(): Int {
+        return listItemsTxt.size
+    }
+
+    private class ItemRowHolder(row: View?) {
+
+        val label: TextView
+
+        init {
+            this.label = row?.findViewById(R.id.txtDropDownLabel) as TextView
+        }
+    }
+
+    fun updateData(newList: ArrayList<ConsentLanguage>){
+        listItemsTxt = newList;
+        notifyDataSetChanged()
+    }
+}
