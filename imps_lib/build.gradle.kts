a7e0768448b93d07a9740159ad9ceeb787f854ab
Ajay Sahu <ajay.sahu@sugaldamani.com>
2025-11-04 16:08:16 +0530

    issue fixed

--- imps_lib/build.gradle.kts
+++ imps_lib/build.gradle.kts
@@ -96,7 +96,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.ajaysaw"
                 artifactId = "imps_lib"
-                version = "1.0.0"
+                version = "1.0.1"
             }
         }
     }
